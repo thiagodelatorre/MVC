514e7462de5ffc09d5d2d8a2954fdd9f9dd4fe8c
Thiago Delatorre <thiago.delatorre@blastradius.com>
2017-03-22 18:45:32 -0400

    Minimal working version.

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult _CTA()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult _Bulletin()
+        {
+            List<BulletPoint> bulletin = new List<BulletPoint>
+            {
+                new BulletPoint { Id=0, Point="Bullet Point one" },
+                new BulletPoint { Id=1, Point="Bullet Point two" },
+                new BulletPoint { Id=2, Point="Bullet Point three" },
+            };
+
+            return PartialView(bulletin);
+        }
+    }
+}
---
+++ MVC/Models/BulletPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class BulletPoint
+    {
+        public int Id { get; set; }
+        public string Point { get; set; }
+    }
+}
