2b22f138117c8404d9ae784a97d6f4987ff9c0a7
Thiago Delatorre <thiago.delatorre@blastradius.com>
2017-03-22 19:16:42 -0400

    Treat BulletPoint as a component

--- MVC/Models/BulletPoint.cs
+++ MVC/Models/BulletPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MVC.Models
     public class BulletPoint
     {
         public int Id { get; set; }
+
+        [DisplayName("Best Movies")]
         public string Point { get; set; }
     }
 }
