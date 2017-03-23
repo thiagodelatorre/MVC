2278bdbb10e1bce8c87e21e45608b139459d27e6
Thiago Delatorre <thiago.delatorre@blastradius.com>
2017-03-22 20:37:48 -0400

    Display a different CTA message at each load

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -18,6 +18,25 @@ namespace MVC.Controllers
         [ChildActionOnly]
         public ActionResult _CTA()
         {
+            Random r = new Random();
+            double chance = r.NextDouble();
+
+            string message = "";
+
+            if(chance < 0.33)
+            {
+                message = "Back-end can apply complex rules to choose the best CTA.";
+            }
+            else if(chance < 0.66)
+            {
+                message = "Can be used for some simple A-B tests and statistics.";
+            } else
+            {
+                message = "May retrieve CTA content from other systems.";
+            }
+
+            ViewBag.message = message;
+
             return PartialView();
         }
 
